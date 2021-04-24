4bcf654665ce181d7a7a752ef983600beba51d54
MD SAZID HASAN DIP <dipcse07@gmail.com>
2021-04-24 20:40:08 +0600

    deeplink is done default deep link

--- DeepLInkDemo/AppDelegate.swift
+++ DeepLInkDemo/AppDelegate.swift
@@ -7,30 +7,45 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+            
+            print("url \(url)")
+            print("url host :\(url.host!)")
+            print("url path :\(url.path)")
+            
+            
+            let urlPath  = url.path
+            let urlHost  = url.host ?? ""
+            let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            
+//            if(urlHost != "somedomain.com")
+//            {
+//                print("Host is not correct")
+//                return false
+//            }
+            
+            if(urlPath == "/inner"){
+                let rootNavController: UINavigationController!
+                rootNavController = UINavigationController(rootViewController: UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "home"))
+                let innerPage = mainStoryboard.instantiateViewController(withIdentifier: "InnerPageViewController") as! InnerPageViewController
+                rootNavController.pushViewController(innerPage, animated: true)
+                self.window?.rootViewController = rootNavController
+            } else if (urlPath == "/about"){
+                
+            }
+            self.window?.makeKeyAndVisible()
+            return true
+        }
 
 }
 
